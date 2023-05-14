ce66ccba3e730ce827466cb94c21afaa878115b1
VictorMFL <vm859222@gmail.com>
2023-05-14 18:13:10 -0300

    add dados individuais de cada filme.

--- src/components/DadosFilmes/DadosFilmes.tsx
+++ src/components/DadosFilmes/DadosFilmes.tsx
@@ -4,8 +4,20 @@ import axios from "axios";
 
 import { autorizacao } from "../../api/api";
 
+import Header from "../Header/Header";
+
+import { FilmeProps } from "../../interface/interface";
+
+import styles from "./DadosFilmes.module.css";
+
+import { Link } from "react-router-dom";
+
+import { GiPopcorn } from "react-icons/gi";
+
+import { BsFillStarFill } from "react-icons/bs";
+
 const DadosFilmes = () => {
-  const [data, setData] = React.useState<any>();
+  const [data, setData] = React.useState<FilmeProps[]>([]);
 
   async function get() {
     const idFilme = window.localStorage.getItem("Filme");
@@ -16,7 +28,7 @@ const DadosFilmes = () => {
       );
       const data = response.data;
       console.log(data);
-      setData(data);
+      setData([data]);
     } catch (error) {
       console.log(error);
     }
@@ -26,7 +38,74 @@ const DadosFilmes = () => {
     get();
   }, []);
 
-  return <div>tuts</div>;
+  if (data.length === 0) return null;
+  return (
+    <>
+      <Header />
+      {data.map((filme) => (
+        <main className={styles.container}>
+          <section className={styles.infoIniciais}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${filme.backdrop_path}`}
+              alt={`Imagem do filme ${filme.title}`}
+            />
+            <h3>Estúdios da criação do filme</h3>
+            {filme.production_companies.length === 0 ? <p className={styles.naoEncontrado}>Não encontrado.</p> : null}
+            {filme.production_companies.map((company) => (
+              <div key={company.id} className={styles.infoCriadores}>
+                {company.logo_path === null ? (
+                  <p className={styles.logoNaoEncontrada}>
+                    Logo não encontrada.
+                  </p>
+                ) : (
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${company.logo_path}`}
+                    alt={`Logo da ${company.name}`}
+                  />
+                )}
+                <p className={styles.nomeCompany}>{company.name}</p>
+              </div>
+            ))}
+
+            <h3 className={styles.titleGeneros}>Gêneros</h3>
+            {filme.genres.map((gen) => (
+              <Link
+                to={`/catalogo-filmes/categorias/${gen.name}`}
+                key={gen.id}
+                className={styles.generos}
+                onClick={() =>
+                  window.localStorage.setItem("Categoria", String(gen.id))
+                }
+              >
+                {gen.name}
+              </Link>
+            ))}
+          </section>
+          <section className={styles.sobre}>
+            <h1>{filme.title}</h1>
+            <p>Descrição do filme: {filme.overview}</p>
+
+            <p className={styles.receita}>Receita Divulgada: $ {filme.revenue} </p>
+
+            <div className={styles.sobreVotos}>
+              <div className={styles.votos}>
+                <p>
+                  Números de votos: <span>{filme.vote_count}</span>
+                </p>
+                <GiPopcorn size={50} color="red" />
+              </div>
+              <div className={styles.votos}>
+                <p>
+                  Média dos votos: <span>{filme.vote_average}</span>
+                </p>
+                <BsFillStarFill size={50} color="yellow" />
+              </div>
+            </div>
+          </section>
+        </main>
+      ))}
+    </>
+  );
 };
 
 export default DadosFilmes;
--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -24,3 +24,51 @@ export interface CategoriaProps {
   id: number;
   name: string;
 }
+
+export interface FilmeProps {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection? : ColectionProps[];
+  budget: number;
+  genres:  Generos[];
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompanies[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface ColectionProps {
+  backdrop_path: string;
+  id: number;
+  name: string;
+  poster_path: string;
+}
+
+interface Generos {
+  id: number;
+  name: string;
+}
+
+interface ProductionCompanies {
+  id: number;
+  logo_path: string | null;
+  name: string;
+  origin_country: string;
+}
+
+interface ProductionCountries {
+  iso_3166_1: string;
+  name: string;
+}
